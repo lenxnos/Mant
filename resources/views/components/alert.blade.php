ab8e4f5c0ab434b2b716986d562c842239981d17
Nelson Soto <nelson.soto.c05@gmail.com>
2017-07-19 09:40:15 -0430

    subiendo todo el proyecto

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,7 @@
+<div class="row">
+	<div class="col-md-8 col-md-offset-2">
+		<div class="alert alert-{{session('alert.type')}}" role="alert">
+			{{ session('alert.message') }}
+		</div>
+	</div>
+</div>
